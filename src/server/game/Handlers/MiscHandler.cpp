d0d25cb15fcc80c1d4a59473b389e1f802654314
SymbolixDEV <shabi_bjk@abv.bg>
2014-02-12 20:57:18 +0200

    Maximum number of player can be displayed in the list/who

--- src/server/game/Handlers/MiscHandler.cpp
+++ src/server/game/Handlers/MiscHandler.cpp
@@ -272,8 +272,9 @@ void WorldSession::HandleWhoOpcode(WorldPacket& recvData)
 
         // check if target's level is in level range
         uint8 lvl = target->getLevel();
-        if (lvl < level_min || lvl > level_max)
+        /*if (lvl < level_min || lvl > level_max)
             continue;
+		*/
 
         // check if class matches classmask
         uint8 class_ = target->getClass();
