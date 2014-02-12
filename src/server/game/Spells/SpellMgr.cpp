cff988b0323da8abac5cbf47099bdd213a80013d
SymbolixDEV <shabi_bjk@abv.bg>
2014-02-12 20:48:47 +0200

    Fix Hunter Cobra Strikes

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3090,6 +3090,10 @@ void SpellMgr::LoadSpellInfoCorrections()
             case 44401: // Missile Barrage
                 spellInfo->ProcCharges = 1;
                 break;
+			case 53257: // Cobra Strikes
+                spellInfo->ProcCharges = 2;
+                spellInfo->StackAmount = 0;
+                break;
             case 44544: // Fingers of Frost
                 spellInfo->Effects[EFFECT_0].SpellClassMask = flag96(685904631, 1151048, 0);
                 break;
